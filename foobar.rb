b73259f0e7fb828ebe5b14a9fef4a932c6d020b5
pkmnfreak <evcchang@berkeley.edu>
2016-09-21 00:06:39 -0700

    Add files via upload

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,17 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    new_list = []
+    sum = 0
+    for i in a;
+    	j = Integer(i) + 2
+    	if j%2 == 0 and j < 10;
+        new_list.push(j)
+      end
+    end
+   	for i in new_list.uniq;
+   		sum += i
+    end
+    return sum
   end
 end
-
-
